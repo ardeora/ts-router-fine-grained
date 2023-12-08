3affb3800df84af355befa68652db221ff7e5483
Aryan Deora <adeora@iu.edu>
2023-12-08 11:43:16 -0500

    Add address bar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,19 +5,128 @@ import {
   redirect,
   useNavigate,
   useParams,
+  useRouter,
+  useSearch,
 } from "@tanstack/react-router";
 import { root } from "./root";
 import { z } from "zod";
 import { motion } from "framer-motion";
-import { memo } from "react";
+import { memo, useEffect } from "react";
 
 const IndexComponent = () => {
   return (
-    <div className="bg-zinc-950 min-h-screen flex p-16 justify-center">
+    <div className="bg-zinc-950 min-h-screen flex flex-col gap-3 pt-8 items-center">
+      <RouteStatus />
       <div className="bg-zinc-900 container max-w-3xl rounded-lg border border-zinc-600 p-6 ">
         <Outlet />
       </div>
     </div>
+  );
+};
+
+function Lock() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M17 11V8A5 5 0 007 8v3m1.8 10h6.4c1.68 0 2.52 0 3.162-.327a3 3 0 001.311-1.311C20 18.72 20 17.88 20 16.2v-.4c0-1.68 0-2.52-.327-3.162a3 3 0 00-1.311-1.311C17.72 11 16.88 11 15.2 11H8.8c-1.68 0-2.52 0-3.162.327a3 3 0 00-1.311 1.311C4 13.28 4 14.12 4 15.8v.4c0 1.68 0 2.52.327 3.162a3 3 0 001.311 1.311C6.28 21 7.12 21 8.8 21z"
+      ></path>
+    </svg>
+  );
+}
+
+const RouteStatus = () => {
+  // const router = useRouter();
+
+  const product_id = useParams({
+    from: "/app/$product_id/$category_id",
+    select(search) {
+      return search.product_id;
+    },
+  });
+
+  const category_id = useParams({
+    from: "/app/$product_id/$category_id",
+    select(search) {
+      return search.category_id;
+    },
+  });
+
+  const product_count = useSearch({
+    from: "/",
+    select: (search) => search.product_count,
+  });
+
+  const category_count = useSearch({
+    from: "/",
+    select: (search) => search.category_count,
+  });
+
+  // const category_count = appRoute.useSearch({
+  //   select: (search) => search.category_count,
+  // });
+
+  // useEffect(() => {
+  //   const unsub = router.subscribe("onBeforeLoad", (p) => {
+  //     console.log("onBeforeLoad", p);
+  //   });
+
+  //   return () => unsub();
+  // }, [router]);
+
+  return (
+    <div className="h-10 bg-zinc-900 rounded-lg container border border-zinc-600 max-w-3xl flex items-center p-4 text-sm text-zinc-400 font-medium">
+      <div className="text-zinc-500">
+        <Lock />
+      </div>
+      <div className="pl-2">acme.com</div>
+      <div className="text-zinc-500 pl-1">/</div>
+      <div className="pl-1">app</div>
+      <div className="text-zinc-500 pl-1">/</div>
+      <RouteLabel key={product_id} label={product_id} />
+      <div className="text-zinc-500 pl-1">/</div>
+      <RouteLabel key={category_id} label={category_id} />
+      <div className="text-zinc-500 pl-1">?</div>
+      <RouteLabel
+        key={product_count}
+        label={`product_count = ${product_count}`}
+      />
+
+      <div className="text-zinc-500 pl-1">&</div>
+      <RouteLabel
+        key={category_count}
+        label={`category_count = ${category_count}`}
+      />
+    </div>
+  );
+};
+
+const RouteLabel = ({ label }: { label: string }) => {
+  return (
+    <motion.div
+      initial={{
+        color: "rgb(74 222 128)",
+      }}
+      animate={{
+        color: "rgb(161 161 170)",
+      }}
+      transition={{
+        duration: 1.5,
+        ease: "easeInOut",
+      }}
+      className="px-1 ml-1 rounded"
+    >
+      {label}
+    </motion.div>
   );
 };
 
